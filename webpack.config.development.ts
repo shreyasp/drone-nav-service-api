26a81ccccfe093cb2f5250dd3347a232ae90ca77
Shreyas Patil <shreyasp@users.noreply.github.com>
2020-06-26 00:04:35 +0530

    Boostrapped the project to Typescript, Webpack etc.

---
+++ webpack.config.development.ts
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: [
+        "webpack/hot/poll?100",
+        "./src/index.ts"
+    ],
+    watch: true,
+    target: "node",
+    externals: [
+        nodeExternals({
+            whitelist: [
+                "webpack/hot/poll?100"
+            ]
+        })
+    ],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                loader: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "index.js"
+    }
+}
