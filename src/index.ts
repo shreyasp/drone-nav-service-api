25e977d53998879d72fc52271b58fb1d4ea94ee0
Shreyas Patil <shreyasp@users.noreply.github.com>
2020-06-26 15:05:38 +0530

    Added ping service. Corrected routing for location, ping

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,75 @@
 /**
  * Required External Modules
  */
-import * as dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import { locationRouter } from './routes/drone.location.routes'
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { locationRouter } from "./routes/drone.location.routes";
+import { pingRouter } from "./routes/ping.service.routes";
 
-dotenv.config()
+dotenv.config();
 
 /**
  * App Variables
  */
 if (!process.env.PORT) {
-    process.exit(1)
+    process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10)
-const app = express()
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const app = express();
 
 /**
  * App Configuration
  */
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 /**
  * Application Routes
  */
-app.use('/v1', locationRouter)
+app.use("/v1/locations", locationRouter);
+app.use("/v1/ping", pingRouter);
 
 /**
  * Health Checker Initialization and Endpoints
  */
-const health = require('@cloudnative/health-connect')
-let healthCheck = new health.HealthChecker()
-app.use('/live', health.LivenessEndpoint(healthCheck))
-app.use('/ready', health.ReadinessEndpoint(healthCheck))
-app.use('/health', health.HealthEndpoint(healthCheck))
+const health = require("@cloudnative/health-connect");
+let healthCheck = new health.HealthChecker();
+app.use("/live", health.LivenessEndpoint(healthCheck));
+app.use("/ready", health.ReadinessEndpoint(healthCheck));
+app.use("/health", health.HealthEndpoint(healthCheck));
 
 /**
  * Server Activation
  */
 const server = app.listen(PORT, () => {
-    console.log(`Server started!! Listening on ${PORT}`)
-})
+    console.log(`Server started!! Listening on ${PORT}`);
+});
 
 /**
  * Webpack HMR Activation
  */
-type ModuleId = string | number
+type ModuleId = string | number;
 
 interface WebpackHotModule {
     hot?: {
-        data: any
-        accept(dependencies: string[], callback?: (updateDependencies: ModuleId[]) => void): void
-        accept(dependency: string, callback?: () => void): void
-        accept(errHandler?: (err: Error) => void): void
-        dispose(callback: (data: any) => void): void
-    }
+        data: any;
+        accept(
+            dependencies: string[],
+            callback?: (updateDependencies: ModuleId[]) => void
+        ): void;
+        accept(dependency: string, callback?: () => void): void;
+        accept(errHandler?: (err: Error) => void): void;
+        dispose(callback: (data: any) => void): void;
+    };
 }
 
-declare const module: WebpackHotModule
+declare const module: WebpackHotModule;
 
 if (module.hot) {
-    module.hot.accept()
-    module.hot.dispose(() => server.close())
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
 }
--- src/routes/drone.location.routes.ts
+++ src/routes/drone.location.routes.ts
@@ -1,20 +1,20 @@
 /**
  * Required External Node Modules
  */
-import express, { Request, Response } from 'express'
-import { getLocation } from '../services/drone.location.services'
-import { DroneParameters } from '../models/drone.location.interface'
-import dotenv from 'dotenv'
+import express, { Request, Response } from "express";
+import { getLocation } from "../services/drone.location.services";
+import { DroneParameters } from "../models/drone.location.interface";
+import dotenv from "dotenv";
 
 /**
  * Configuration
  */
-dotenv.config()
+dotenv.config();
 
 /**
  * Router Definition
  */
-const locationRouter = express.Router()
+const locationRouter = express.Router();
 
 /**
  * Controller Definitions
@@ -22,30 +22,30 @@ const locationRouter = express.Router()
 
 // Get Location based on drone parameters
 
-locationRouter.get('/locations', async (req: Request, resp: Response) => {
+locationRouter.get("/", async (req: Request, resp: Response) => {
     const droneParams: DroneParameters = {
         x: parseFloat(req.body.x as string),
         y: parseFloat(req.body.y as string),
         z: parseFloat(req.body.z as string),
         v: parseFloat(req.body.vel as string),
-    }
+    };
 
     let sectorId: number =
-        process.env.GLOBAL_SERVICE === 'true'
+        process.env.GLOBAL_SERVICE === "true"
             ? parseInt(req.body.sectorId as string, 10)
-            : parseInt(process.env.SECTOR_ID as string, 10)
+            : parseInt(process.env.SECTOR_ID as string, 10);
 
     try {
-        const location: String = await getLocation(droneParams, sectorId)
+        const location: String = await getLocation(droneParams, sectorId);
         resp.status(200).json({
             location,
-        })
+        });
     } catch (err) {
         resp.status(500).json({
             message: err.message,
             stack: err.stack,
-        })
+        });
     }
-})
+});
 
-export { locationRouter }
+export { locationRouter };
